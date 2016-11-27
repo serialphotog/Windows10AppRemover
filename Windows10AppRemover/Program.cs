a5faa728eeb620a9db73666f5bec7d85a7aa9b8a
Adam Thompson <adam@serialphotog.com>
2016-11-26 20:17:05 -0500

    Licensing and code cleanup

--- Windows10AppRemover/Program.cs
+++ Windows10AppRemover/Program.cs
@@ -1,7 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿/**
+ * Copyright © 2016 Adam Thompson <adam@serialphotog.com>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See the COPYING file for more details.
+ */
+
+using System;
 using System.Windows.Forms;
 
 namespace Windows10AppRemover
